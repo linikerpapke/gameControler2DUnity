60ca8f5800dcf78fb5e84c8666fcb3f50c4f528b
Liniker Papke <papke.liniker@gmail.com>
2022-03-02 15:35:46 -0300

    first commit

---
+++ Player.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float Speed;
+    public float JumpForce;
+
+    public bool isJumping;
+    public bool doubleJump;
+
+    private Rigidbody2D rig;
+    private Animator anim;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * Speed;
+
+        // Virando da direita pra esquerda
+
+        if (Input.GetAxis("Horizontal") > 0f)
+        {
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+
+        }
+
+        if (Input.GetAxis("Horizontal") < 0f)
+        {
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+
+        }
+
+        if (Input.GetAxis("Horizontal") == 0f)
+        {
+            anim.SetBool("walk", false);
+
+        }
+    }
+
+    //se quiser dar só um pulo -> if(Input.GetButtonDown("Jump") && !isJumping)
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+
+        {
+            if(!isJumping)
+            {
+                rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                //usar double jump só para pulo duplo
+                doubleJump = true;
+                anim.SetBool("jump", true);
+            }
+            else
+            {     
+                if(doubleJump)
+                {
+                    rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                    doubleJump = false;
+                }
+            }
+
+            
+        }
+    }
+
+    //identificar se está no Ground - para Pulo Duplo
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            isJumping = false;
+            anim.SetBool("jump", false);
+
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            isJumping = true;
+        }
+    }
+}
